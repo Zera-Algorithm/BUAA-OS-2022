4d7f2bb10706b833c53e3d83edc5eafba0e16905
20373649 <20373649@buaa.edu.cn>
2022-03-17 14:56:34 +0800

    lab0-Extra-attempt1

---
+++ find_warnings.sh
@@ -0,0 +1,13 @@
+gcc -Wall $1 -o test 2> warning.txt
+a=$?
+grep 'warning: ' warning.txt | sed 's/warning: //g' > result.txt
+if [ $a -eq 0 ]
+then
+	i=1
+	while [ $i -le $2 ]
+	do
+		./test $i >> result.txt
+		i=$[$i+1]
+	done
+fi
+pwd >> result.txt
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	int a,b;
+	a = 2;
+	printf("Hello World!\n");
+	return 0;
+}
