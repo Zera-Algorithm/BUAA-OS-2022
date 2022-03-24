dbb3b2c87b945698316b9a53bc74b4fe2157b0a8
20373649 <20373649@buaa.edu.cn>
2022-03-24 15:24:49 +0800

    try1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -43,17 +43,21 @@ int is_elf_format(u_char *binary)
 /*
     Exercise 1.2. Please complete func "readelf". 
 */
+int cmp(void *a, void* b) {
+	Elf32_Phdr *x = (Elf32_Phdr *)a;
+	Elf32_Phdr *y = (Elf32_Phdr *)b;
+	return (x->p_vaddr - y->p_vaddr);
+}
+
 int readelf(u_char *binary, int size)
 {
         Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
 
         int Nr;
 
-        Elf32_Shdr *shdr = NULL;
-
-        u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count;
-        Elf32_Half sh_entry_size;
+        Elf32_Phdr *phdr = NULL;
+        Elf32_Half ph_entry_count;
+        Elf32_Half ph_entry_size;
 
 
         // check whether `binary` is a ELF file.
@@ -63,19 +67,42 @@ int readelf(u_char *binary, int size)
         }
 
         // get section table addr, section header number and section header size.
-	Elf32_Off shoff = ehdr->e_shoff;
-	sh_entry_count = ehdr->e_shnum;
-	sh_entry_size = ehdr->e_shentsize;
+	Elf32_Off phoff = ehdr->e_phoff;
+	ph_entry_count = ehdr->e_phnum;
+	ph_entry_size = ehdr->e_phentsize;
         // for each section header, output section number and section addr. 
         // hint: section number starts at 0.
-	shdr = (Elf32_Shdr *)(binary + shoff);
+	phdr = (Elf32_Phdr *)(binary + phoff);
 	int i;
-	for (i = 0; i < sh_entry_count; i++) {
-		Elf32_Addr section_addr = shdr->sh_addr;
-		printf("%d:0x%x\n", i, section_addr);
-		shdr += 1;
+	for (i = 0; i < ph_entry_count; i++) {
+		(phdr + i) -> p_type = (Elf32_Word)i;
 	}
+	qsort( (void *)phdr, (size_t)ph_entry_count, sizeof(Elf32_Phdr), cmp);
+	
+	for (i = 0; i < ph_entry_count-1; i++) {
+		Elf32_Addr l1 = phdr[i].p_vaddr;
+		Elf32_Word memsz1 = phdr[i].p_memsz;
+		Elf32_Addr r1 = l1 + memsz1 - 1;
 
-        return 0;
+		Elf32_Addr l2 = phdr[i+1].p_vaddr;
+        Elf32_Word memsz2 = phdr[i+1].p_memsz;
+        Elf32_Addr r2 = l2 + memsz2 - 1;
+
+		if( (r1 & (0xfffff000)) == (l1 & (0xfffff000)) ) {
+			if (r1 < l1) {
+				// Overlay
+				printf("Overlay at page va : 0x%x\n", l1);
+			}
+			else {
+				// Conflict
+				printf("Conflict at page va : 0x%x\n", l1);
+			}
+			i += 1;
+		}
+		else {
+			printf("%d:0x%x,0x%x\n", phdr[i].p_type, phdr[i].p_filesz, phdr[i].p_memsz);
+		}
+	}
+    return 0;
 }
 
