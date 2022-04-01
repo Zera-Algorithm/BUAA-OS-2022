eaba158fb608039f3c69099bf1bce10073b7016a
GitLab <gitlab@git.os.buaa.edu.cn>
2022-04-01 11:12:41 +0800

    Initial lab2

--- init/main.c
+++ init/main.c
@@ -12,28 +12,9 @@
 #include <printf.h>
 #include <pmap.h>
 
-void test() {
-	printf("Binary = %b, Decimal = %d, Octal = %O, Unsigned = %u\n", 14, 455, 233, (int)(-56));
-	// illegal inputs
-	printf("This is an illegal input: %l, %ll, %.w\n");
-	printf("Sizeof(long) = %d\n", sizeof(long int));
-	printf("long num = %ld\n", (long int)10000l);
-	printf("ladleft = %-5dThis is a.\n", 23);
-	printf("rightlad = %7dThis is a.\n", 23456);
-	printf("overwidth = %2dThis is a.\n", 12345);
-	printf("Char = %.2c.\n", 'w');
-	printf("Simple negative num = %d\n", (int)(-342));
-	printf("0leftpad = %06.4d\n", (int)(-12));
-	printf("%0----0-0-4dhello.\n", (int)23);
-}
-
 int main()
 {
 	printf("main.c:\tmain is start ...\n");
-
-	/* This code is only for printf testing! */
-	// test();
-	/* This is the end */
 
 	mips_init();
 	panic("main is over is error!");
