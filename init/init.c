1d94f521f2982239c436d5cd41d976ad114d2161
20373649 <20373649@buaa.edu.cn>
2022-05-17 16:36:48 +0800

    modify init.c

--- init/init.c
+++ init/init.c
@@ -15,8 +15,8 @@ void mips_init() {
 	env_init();
 
 	//ENV_CREATE(user_tltest);
-	ENV_CREATE(user_fktest);
-	// ENV_CREATE(user_pingpong);
+	// ENV_CREATE(user_fktest);
+	ENV_CREATE(user_pingpong);
 
 	trap_init();
 	kclock_init();
