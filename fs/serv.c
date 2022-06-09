9438e82d3ee59de9e93cc770ab9f63d569c9e8c1
20373649 <20373649@buaa.edu.cn>
2022-06-09 14:55:20 +0800

    change O_ALONE implement

--- fs/serv.c
+++ fs/serv.c
@@ -176,12 +176,12 @@ serve_map(u_int envid, struct Fsreq_map *rq)
 		return;
 	}
 
-	if (pOpen->o_mode & O_ALONE) {
-		ipc_send(envid, 0, (u_int)blk, PTE_V | PTE_R);
-	}
-	else {
+	// if (pOpen->o_mode & O_ALONE) {
+	// 	ipc_send(envid, 0, (u_int)blk, PTE_V | PTE_R);
+	// }
+	// else {
 		ipc_send(envid, 0, (u_int)blk, PTE_V | PTE_R | PTE_LIBRARY);
-	}
+	// }
 }
 
 void
