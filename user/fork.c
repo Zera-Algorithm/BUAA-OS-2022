adead7ac711f06a94dc802315a5851296f44028e
20373649 <20373649@buaa.edu.cn>
2022-06-09 15:21:01 +0800

    change fork

--- user/fork.c
+++ user/fork.c
@@ -101,6 +101,10 @@ pgfault(u_int va)
 
 	// writef("Pgfault: curenv->envid = %d.\n", env_id);
 	
+	if (pageref(va) == 1) {
+		(*vpt)[pn] ^= PTE_COW;
+	}
+
 	// writef("2. Stack saved ra = %x.\n", *(int *)(0x7f3fdfbc));
 	// don't use env->env_id, it's not correct! Pgfault may happens in syscall_getenvid.
 	r = syscall_mem_alloc(env_id, tmp, perm ^ PTE_COW);
