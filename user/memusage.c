7527d451b8c8ce202e51d8f924cb9846e7e30fa7
20373649 <20373649@buaa.edu.cn>
2022-07-03 18:06:29 +0800

    memusage update

--- user/memusage.c
+++ user/memusage.c
@@ -3,11 +3,12 @@
 void umain() {
     int i, r, used = 0;
     int npages = 64 * 1024 * 1024 / 4096;
+    writef("Total page count = %d\n", npages);
     for (i = 0; i < npages; i++) {
         if (pages[i].pp_ref != 0) {
             used += 1;
         }
     }
-    int kb = (double) used / npages * 64 * 1024;
+    int kb = used * 4;
     writef("Used Mem: %d KB.\n", kb);
 }
